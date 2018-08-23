ff93e44c28b12ed562578f340df17359ad42872d
Anthony <ajxscc@gmail.com>
2018-08-24 09:02:38 +1000

    PSQL setup script

---
+++ setup-psql-arch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./_shared.sh
+
+
+# perldoc comes loaded with Perl on Arch.
+if [[ "${__DETECTED_SYSTEM__}" != "ARCH" ]]; then
+	die_with_message "Script intended to be run on ARCH! Exiting."
+fi
+
+check_install_package "postgresql"
+
+# Setup postgres data dir.
+PSQL_DATA_DIR="/var/lib/postgres/data"
+
+sudo mkdir "${PSQL_DATA_DIR}"
+sudo chmod 775 "${PSQL_DATA_DIR}"
+sudo chown postgres "${PSQL_DATA_DIR}"
+
+# Postgres init.
+sudo -u postgres -i initdb --locale en_US.UTF-8 -E UTF8 -D ${PSQL_DATA_DIR}
+sudo -u postgres -i pg_ctl -D ${PSQL_DATA_DIR} -l logfile start
+
+# Start service.
+sudo systemctl start postgresql.service
